8c6ca0eed83c2d45d1305f74ab37a77d987a403c
Eduardofig <duzinho039@gmail.com>
2021-04-20 20:03:00 +0000

    feh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "starting"
-sudo apt install neovim kitty xcape x11-xserver-utils fish npm clangd
+sudo apt install neovim kitty xcape x11-xserver-utils fish npm clangd feh
 mv ~/.config .config.old
 mv ~/arch-config/.config .config
 setxkbmap -option ctrl:nocaps
@@ -15,4 +15,6 @@ xset r rate 210 40
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 feh --bg-scale ~/arch-config/Walpapers/wallpaper 3.jpeg
+git config --global user.email "duzinho039@gmail.com"
+git config --global user.name "Eduardofig"
 echo "done"
